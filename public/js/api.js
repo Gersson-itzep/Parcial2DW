ce4921b46a17ff84e4dd786d0d44447f8a484945
Gersson-itzep <37203156+Gersson-itzep@users.noreply.github.com>
2021-09-18 17:46:24 -0600

    Add files via upload

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "01d90e9b041152337a45066f85a96770";
+let img_url = "https://image.tmdb.org/t/p/w500";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
+let lenguaje_api= "https://api.themoviedb.org/4/list/1?language=es"; 
